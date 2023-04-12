e90d25836dd3fbbcef68f57ba224f18fb8fc0220
zengzzzzz <mrzengzzzz@gmail.com>
2023-04-12 17:10:40 +0800

    Create git-script.sh

---
+++ git-script.sh
@@ -0,0 +1,19 @@
+set -ex   # add set -ex
+
+# delete the temporary file
+ls -d .* | grep -E "^\.[0-9a-fA-F]{32}$" | xargs rm
+
+# determine message
+if [ -z "$1" ]
+then
+    echo "Commit message is required."
+    exit 1
+else
+    message=$1
+fi
+
+# add changes to local respoisity
+git add .
+
+# push to remote respoisity
+git commit --allow-empty -m "$message"
